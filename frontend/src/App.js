337e45088273fd64a78309c7d6e10dd894a0fa35
shyboy443 <ashen.semithu@gmail.com>
2024-01-20 15:35:02 +0530

    front end commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,52 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
+import Sidebar from "./components/sidebar/Sidebar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Layout from "./components/layout/Layout";
+import axios from "axios";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
+axios.defaults.withCredentials  = true;
+
+
 
 function App() {
   return (
-    <div>
-    <h2>hiii</h2>
-       
-    </div>
+
+    <BrowserRouter>
+     <ToastContainer />
+    <Routes>
+      <Route path = "/" element={<Home/>}/>
+      <Route path = "/login" element={<Login/>}/>
+      <Route path = "/register" element={<Register/>}/>
+      <Route path = "/forgot" element={<Forgot/>}/>
+      <Route path = "/resetpassword/:resetToken" element={<Reset/>}/>
+
+
+      <Route path="/dashboard" element = {
+          <Sidebar>
+
+          
+            <Layout>
+              <Dashboard/>
+
+            </Layout>
+            </Sidebar>
+
+      
+      }/>
+
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+ export const BACKEND_URL = process.env.BACKEND_URL
+
+export const validateEmail = (email) => {
+    return email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+}
+
+// Register 
+
+
+export const registerUser = async(userData) => {
+    try {
+        const response = await axios.post("http://localhost:2000/api/users/register",userData,
+        {withCredentials:true})
+
+        
+           toast.success("Registered Successfully")
+            return response.data;
+
+    } catch (error) {
+        const message = (
+            error.response && error.response.data && error.response.data.message
+        ) || error.message || error.toString();
+        toast.error(message)
+    }
+
+
+}
+
+
+// Login
+
+export const loginUser = async(userData) => {
+    try {
+        const response = await axios.post("http://localhost:2000/api/users/login",userData,
+        )
+        if(response.statusText === "OK"){
+        
+           toast.success("Login  Success")
+            return response.data;
+    }
+    } catch (error) {
+        const message = (
+            error.response && error.response.data && error.response.data.message
+        ) || error.message || error.toString();
+        toast.error(message)
+    }
+
+
+}
+
+// Logout
+export const logoutUser = async () => {
+    try {
+      await axios.get(`http://localhost:2000/api/users/logout`);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+    }
+  };
+
+  // Forgot Password
+
+export const forgotPassword = async (userData) => {
+    try {
+        const response =  await axios.post(`http://localhost:2000/api/users/forgotpassword`,userData);
+        toast.success(response.data.message)
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+    }
+  };
+
+  // Reset Password
+export const resetPassword = async (userData,resetToken) => {
+    try {
+        const response =  await axios.put(`http://localhost:2000/api/users/resetpassword/${resetToken}`,userData);
+    
+
+        return response.data
+
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+    }
+  };
